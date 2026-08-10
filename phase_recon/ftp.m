ab41b3a03c73f90772f518fd8d1228cb2f2ce6fc
Gong HB <Gong HB@DESKTOP-0V4MO6J>
2026-08-10 20:51:01 +0800

    ftp

---
+++ phase_recon/ftp.m
@@ -0,0 +1,15 @@
+function phi = ftp(X, Y, I, fc, win_size, n_noll)
+[Ny, Nx] = size(I);
+dx = Y(2)-Y(1);
+I_fft = fftshift(fft2(I));
+fx = (-Nx/2:Nx/2-1) * (1/Nx/dx);
+fy = (-Ny/2:Ny/2-1) * (1/Ny/dx);
+[FX, FY] = meshgrid(fx, fy);
+mask = exp(-sqrt((FX-fc(1)).^2+(FY-fc(2)).^2)/win_size).^2;
+I_fft_filtered = I_fft .* mask;
+phi = angle(ifft2(ifftshift(I_fft_filtered)));
+phi = unwrap2D(phi);
+coef = rect_zernike_coef(X,Y,phi,1:n_noll);
+coef(1:3) = 0;
+phi = rect_zernike_recon(X, Y, coef);
+end
---
+++ tests/test_ftp.m
@@ -0,0 +1,53 @@
+clear
+
+dx = 4.5;
+Nx = 1920;
+Ny = 1080;
+x = (-Nx/2:Nx/2-1) * dx;
+y = (-Ny/2:Ny/2-1) * dx;
+[X, Y] = meshgrid(x, y);
+
+coef0 = zeros(1, 20);
+coef0(12) = 1;
+coef0 = coef0 + 0.1*rand(1, 20);
+coef0(1:3) = 0;
+
+phi0 = rect_zernike_recon(X, Y, coef0);
+
+fx_carry = 1/.45*sind(.5);
+I = abs(exp(1j*phi0) + exp(-1j*2*pi*fx_carry*X)).^2;
+% I = awgn(I, 3);
+
+phi_recon = ftp(X, Y, I, [fx_carry, 0], 0.01, 20);
+%%
+figure
+imagesc(x, y, I)
+colormap("gray")
+colorbar
+grid on
+axis image
+
+figure
+subplot(311)
+imagesc(x, y, phi0)
+colormap("hot")
+colorbar
+axis image
+title("Phase Ground")
+subplot(312)
+imagesc(x, y, phi_recon)
+colorbar
+axis image
+title("Phase Recovered")
+subplot(313)
+imagesc(x, y, abs(phi_recon-phi0))
+colorbar
+axis image
+title("Phase Error")
+
+error_in_wavelength = sqrt(mean((phi_recon-phi0).^2, "all")) / (2*pi);
+sprintf("Error: %3E wavelength", error_in_wavelength)
+
+
+
+
